001b31d5eca55544fbd3487b72911abbbba4f1f7
Nicolas Marty <nicolas.marty@zuehlke.com>
2021-09-27 02:44:24 -0700

    add raspberry pi specific build configuration

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
+# general config
+PROJECT_PATH="$PWD"
+
 # install dependencies
-sudo apt install -y bc build-essential chrpath cpio diffstat gawk git python texinfo wget
+sudo apt install -y bc build-essential chrpath cpio diffstat gawk git python texinfo wget python3-distutils
 
 # checkout meta-layers
 git clone --depth 1 git://git.yoctoproject.org/poky.git --branch dunfell --single-branch
 git clone --depth 1 git://git.yoctoproject.org/meta-raspberrypi.git --branch dunfell --single-branch
 git clone --depth 1 git://git.openembedded.org/meta-openembedded --branch dunfell --single-branch
 
+# init build
+source poky/oe-init-build-env
+
+# overwrite auto generated config with own config
+pushd $PROJECT_PATH
+cp -rf conf/local.conf build/conf/local.conf
+cp -rf conf/bblayers.conf build/conf/bblayers.conf
+popd
+
+
+# build minimal image
+bitbake core-image-minimal
