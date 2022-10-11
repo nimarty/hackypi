6f2444e0c93d9ce7f1b93ec2366df88aa86dfdc9
Nicolas Marty <nicolas.marty@zuehlke.com>
2022-10-11 11:24:19 -0400

    remove comment

--- meta-hackypi/recipes-vulnerable/deceived-donald/files/src/memfunctions/memfunctions.h
+++ meta-hackypi/recipes-vulnerable/deceived-donald/files/src/memfunctions/memfunctions.h
@@ -2,13 +2,6 @@
 
 namespace MemFunctions {
 
-    //////////////////////////////////////////////////////////////////////////////
-    //
-    // process_mem_usage(double &, double &) - takes two doubles by reference,
-    // attempts to read the system-dependent data for a process' virtual memory
-    // size and resident set size, and return the results in KB.
-    //
-    // On failure, returns 0.0, 0.0
     void process_mem_usage(double& vm_usage, double& resident_set);    
 
     long system_mem_free();
