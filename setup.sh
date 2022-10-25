19122eb9315e1f308d4872786c8c8f6eeb50d217
Nicolas Marty <nicolas.marty@zuehlke.com>
2022-10-25 12:35:32 -0400

    cleanup setup script

--- setup.sh
+++ setup.sh
@@ -7,24 +7,9 @@ PROJECT_PATH="$PWD"
 sudo apt install -y -qq bc build-essential chrpath cpio diffstat gawk git texinfo wget python3-distutils chrpath diffstat repo || exit 1
 
 # checkout meta-layers
-#git clone --depth 10 git://git.yoctoproject.org/poky.git --branch dunfell --single-branch
-#pushd poky
-#git reset --hard 7f9b7f912e13451a4cd03b10a8090a5def68dc39 || exit 1
-#popd
-#
-#git clone --depth 1 git://git.yoctoproject.org/meta-raspberrypi.git --branch dunfell --single-branch
-#pushd meta-raspberrypi
-#git reset --hard 2081e1bb9a44025db7297bfd5d024977d42191ed || exit 1
-#popd
-#
-#git clone --depth 1 git://git.openembedded.org/meta-openembedded --branch dunfell --single-branch
-#pushd meta-openembedded
-#git reset --hard 6792ebdd966aa0fb662989529193a0940fbfee00 || exit 1
-#popd
-
 repo init -q --depth 10 -m manifest.xml -b enhancement/define-yocto-releae https://github.com/nimarty/hackypi || exit 1
 cp -rf manifest.xml .repo/ || exit 1 # overwrite with local manifest
-repo sync -q || exit 1
+repo sync || exit 1
 
 # init build
 source poky/oe-init-build-env &> /dev/null || exit 1
